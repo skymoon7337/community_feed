939caea1673632a61ceb6744bbec7db7b722e7e9
skymoon7337 <skymoon7337@gmail.com>
2025-09-23 23:16:36 +0900

    JAVA_OOD - post, comment

---
+++ src/main/java/org/fastcampus/user/domain/UserInfo.java
@@ -0,0 +1,18 @@
+package org.fastcampus.user.domain;
+
+public class UserInfo {
+
+    private final String name;
+    private final String profileImageUrl;
+
+    public UserInfo(String name, String profileImageUrl) {
+
+        // 유효성 검증
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
