da03a9ad4cdc37856586e8160da9d9a6fa791992
skymoon7337 <skymoon7337@gmail.com>
2025-10-05 21:48:06 +0900

    Api- post, comment

--- src/main/java/org/skymoon7337/post/repository/PostRepositoryImpl.java
+++ src/main/java/org/skymoon7337/post/repository/PostRepositoryImpl.java
@@ -14,7 +14,7 @@ public class PostRepositoryImpl implements PostRepository {
 
     private final JpaPostRepository jpaPostRepository;
 
-    // new PostEntity(post)로 만들어서 postId(pk)가 존재, JPA가 관리하는 객체 X(새로 만든 객체)
+    // new PostEntity(post)로 만들어서 postId(pk)가 존재, JPA가 관리하는 객체 X (새로 만든 객체)
     // 이 PostEntity 객체를 조회한적이 없어서 DB에 SELECT(확인) 필요함 -> merge() 실행됨 (SELECT + UPDATE) // (ID가 없으면 → persist()-> INSERT, ID가 있으면 → merge() -> SELECT + UPDATE 실행)
     // 총 SELECT 3번, UPDATE 1번 = 총 4번의 쿼리 발생 // (PostService에서 updatePost에서 2번 SELECT)
     // *문제 -> 같은 Post 재조회 (getPost, merge) -> JpaPostRepository에 쿼리 작성으로 해결 (여러 방법 존재)
